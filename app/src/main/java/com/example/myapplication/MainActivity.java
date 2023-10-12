01ca1aa3bbeb7fe307cf06c4c31863c731cb5ae9
ireshsits <131250283+ireshsits@users.noreply.github.com>
2023-10-12 18:56:01 +0530

    10-2-EOD

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -96,6 +100,48 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, PreCompletionActivity.class); // Replace NewActivity with the name of your new activity class
                 startActivity(intent);
+            }
+        });
+
+
+
+        showCustomAlertDialog();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private void showCustomAlertDialog() {
+
+
+
+        View customView =LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_dialog, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setView(customView);
+
+        ImageView cancel = customView.findViewById(R.id.btnCancelTop);
+
+
+        final AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
             }
         });
 
